42cac1e2ed0afa8be4202b09f726e0c1aebf9876
JacobR96 <jrosswalks@gmail.com>
2023-03-30 04:25:52 -0700

    First commit, implamenting both buttons and the input box

---
+++ preparedness-frontend/preparedness-assessment/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import MyButton from './Components/frontpage';
+import './App.css'
+import Example from './Components/model';
+
+function App() {
+  return (<>
+  <body>  
+     
+ <MyButton/>
+ <Example/>
+ 
+  </body>
+  </> )
+}
+
+export default App;
