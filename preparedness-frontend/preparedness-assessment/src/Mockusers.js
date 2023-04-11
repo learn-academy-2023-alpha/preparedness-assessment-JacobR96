5f6b56e85fd2755a4114a42f967c3ea5587f330a
JacobR96 <jrosswalks@gmail.com>
2023-04-11 08:44:17 -0700

    finished front page and started on DB

---
+++ preparedness-frontend/preparedness-assessment/src/Mockusers.js
@@ -0,0 +1,40 @@
+const users = [
+  {
+    id: 1,
+    name: "Alice",
+    age: 25,
+    enjoys: "Hiking and reading",
+    image: "https://example.com/alice.jpg",
+  },
+  {
+    id: 2,
+    name: "Bob",
+    age: 32,
+    enjoys: "Playing guitar and cooking",
+    image: "https://example.com/bob.jpg",
+  },
+
+  {
+    id: 3,
+    name: "Charlie",
+    age: 19,
+    enjoys: "Playing video games and watching movies",
+    image: "https://example.com/charlie.jpg",
+  },
+
+  {
+    id: 4,
+    name: "Diana",
+    age: 41,
+    enjoys: "Traveling and trying new foods",
+    image: "https://example.com/diana.jpg",
+  },
+  {
+    id: 5,
+    name: "Eva",
+    age: 28,
+    enjoys: "Swimming and playing tennis",
+    image: "https://example.com/eva.jpg",
+  },
+];
+export default users;
